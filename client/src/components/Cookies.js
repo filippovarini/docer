ec3d4ec98b4d9881c5c2076809309c669de1c32a
fv220 <fv220@ic.ac.uk>
2022-02-05 23:22:36 +0000

    feat: fake cookies

--- client/src/components/Cookies.js
+++ client/src/components/Cookies.js
@@ -5,12 +5,17 @@ export class Cookies extends Component {
   render() {
     return (
       <div id="cookies">
-        <p id="delete">x</p>
-        Eu consectetur dolor sunt ut consequat. Magna nisi elit ut qui excepteur
-        pariatur mollit quis esse irure excepteur magna. Nisi dolor ipsum fugiat
-        irure laboris adipisicing qui commodo officia consectetur ut qui officia
-        anim. Eiusmod aliqua anim eu dolor. Non qui ad id eu. Quis anim culpa
-        occaecat ex magna qui quis labore ut tempor sit duis.
+        We value your privacy. We and our partners use trackers to measure the
+        audience of our website and to provide you with offers and improve our
+        own Tinder marketing operations.
+        <i style={{ textDecoration: "underline" }}>
+          More info on cookies and providers we use.
+        </i>{" "}
+        You can withdraw your consent at any time in your settings.
+        <div id="buttons">
+          <p className="button">I ACCEPT</p>
+          <p className="button">PERSONALIZE MY CHOICES</p>
+        </div>
       </div>
     );
   }
